b4b8f437130783afb85c010ea8d702e79234eae5
ashish <aashidare123@gmail.com>
2024-08-21 08:58:13 +0530

    data types

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId= 11
+let accountName="Ashish"
+var accountCity="Bbsr"
+accountState= "Odisha"
+
+let accountNo;
+// accountId= 20
+
+accountName="Shyam"
+accountCity="Jsr"
+accountState="Jharkhand"
+
+// console.log(accountId);
+
+/*
+Only use const and let while declaring variables don't use var
+Because of block level and functional level scope
+
+
+*/
+
+console.table([accountName,accountCity,accountState,accountId,accountNo]);
+
+
