5b04d4b4344deff9ae7ff5139b673348b97aa2e3
ashish <aashidare123@gmail.com>
2024-08-25 21:01:54 +0530

    datatypes and data-conversion 	new file:   01_Basics/02_datatypes.js 	new file:   01_Basics/03_dataconversion.js

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict" // it is used to treat js code as newer version
+// use tc39 and mdn for documentation
+
+//alert(3+3)// this is not work in nodejs'
+
+// console.log(3
+//     +3);
+
+//     console.log("Ashish");
+
+
+let age=18
+let isLoggedIn= false
+let state;
+
+
+//Primitive datatypes
+//number- 2 power 53
+//bigint
+//string=> ""
+//boolean=> true/false
+//null==> standalone value
+//undefined=> variable declared but not assigned
+//symbols=> used for uniqueness of an component, reactjs figma
+    
+//object
+
+console.log(typeof "");
+console.log(typeof null);
+console.log(typeof undefined);
+//typeof is used to know the datatype of that value or variable
+
+console.log(age);
+console.log(isLoggedIn);
+console.log(state);
+
---
+++ 01_Basics/03_dataconversion.js
@@ -0,0 +1,114 @@
+let score= 33
+
+let score1="33"
+let score2="33abc"
+
+
+
+// console.log( typeof score);
+// console.log( typeof score1);
+// console.log( typeof score2);
+
+let valueNumber= Number(score1)
+let valueNumber1= Number(score2)
+// console.log(typeof valueNumber);
+
+// console.log(typeof valueNumber1);
+
+// console.log( valueNumber);
+// console.log( valueNumber1);
+let score3= null;
+let score4= undefined;
+let score5= true;
+let valueNumber2= Number(score3)
+let valueNumber4= Number(score4)
+let valueNumber5= Number(score5)
+
+
+//  console.log( valueNumber2);
+//  console.log( valueNumber3);
+//   console.log( valueNumber4);
+//   console.log( valueNumber5);
+
+//  console.log(typeof valueNumber2);
+//  console.log(typeof valueNumber3);
+//  console.log(typeof valueNumber3);
+//   console.log(typeof valueNumber4);
+//   console.log(typeof valueNumber5);
+
+/************************************** */
+
+//String Conversion
+
+
+let value =33
+let value1= true
+let value2= null
+let value3= undefined
+
+let Value= String(value)
+let Value2= String(value1)
+let Value3= String(value2)
+let Value4= String(value3)
+
+// console.log(Value);
+// console.log(Value2);
+// console.log(Value3);
+// console.log(Value4);
+// console.log( typeof Value);
+// console.log( typeof Value2);
+// console.log( typeof Value3);
+// console.log( typeof Value4);
+
+
+
+// Boolean Conversion
+
+
+
+let valueBool= 123
+let valueBool2= ""
+let valueBool3= "ashish"
+let valueBool4= 1
+let valueBool5= 0
+let valueBool6= null
+let valueBool7= undefined
+
+let boolvalue= Boolean(valueBool)
+let boolvalue1= Boolean(valueBool2)
+let boolvalue2= Boolean(valueBool3)
+let boolvalue3= Boolean(valueBool4)
+let boolvalue4= Boolean(valueBool5)
+let boolvalue5= Boolean(valueBool6)
+let boolvalue6= Boolean(valueBool7)
+
+
+
+console.log(boolvalue);
+console.log(boolvalue1);
+console.log(boolvalue2);
+console.log(boolvalue3);
+console.log(boolvalue4);
+console.log(boolvalue5);
+console.log(boolvalue6);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
