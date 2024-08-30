07479545bcf33c35605c03f3a5f0e400d811ea07
ashish <aashidare123@gmail.com>
2024-08-31 00:14:48 +0530

    for loop

---
+++ 05_Iterations/one.js
@@ -0,0 +1,55 @@
+// for (let i = 0; i <= 10; i++) {
+//     const element = i
+//     console.log(i);
+    
+    
+// }
+
+
+
+
+
+// for (let i = 1; i <=10; i++) {
+
+//     console.log(`Outer loop : ${i}`);
+    
+//    for (let j = 1; j <=10;  j++) {
+
+//     // console.log(`inner loop is ${j} and outer loop is ${i}`);
+
+//     console.log(" "+i +"* "+j+" = "+i*j+"");
+    
+    
+   
+    
+//    }
+    
+// }
+
+
+
+
+for (let i = 1; i <=10; i++) {
+
+
+    if(i===5){
+
+        console.log("5 detected");
+
+        // break
+        continue
+    }
+
+    console.log(`value: ${i}`);
+    
+  
+    
+}
+
+
+// break statement will terminate the loop and comes out of it, code after that will not to be executed
+// continue statement will skip that iteration and move on the next iteration
+
+
+
+
