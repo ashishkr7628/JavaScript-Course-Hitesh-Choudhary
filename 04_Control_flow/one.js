532605aaaa1cbaf15d1be6eab126a3949f8d2427
ashish <aashidare123@gmail.com>
2024-08-30 09:18:09 +0530

    if-else

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,147 @@
+// if(true){
+
+//     console.log("Hii Ashish");
+    
+// }
+
+
+// if(false){
+
+//     console.log("hello dear");
+    
+// }
+
+
+// const name="ashish"
+// if(name){
+
+//     console.log(`My name is ${name}`);
+    
+// }
+
+
+// const isUserLogged =true
+
+// if(isUserLogged){
+
+
+//     console.log("Welcome Back");
+    
+// }
+
+
+
+// <,>,==,<=,>=,===,!==
+
+//=== -> it will check both value and datatype
+
+// if(2<=2){
+
+//     console.log("hii");
+    
+// }
+
+// if(2==="2"){
+//     console.log("hello");
+    
+// }
+// else{
+//     console.log("bye");
+    
+// }
+
+
+// if((0.1+0.2)==0.3){
+//     console.log("hello");
+    
+// }
+
+const temp=40
+
+// if(temp>50){
+
+//     console.log("greater than 50");
+    
+// }
+
+// else{
+
+//     console.log("less than 50");
+    
+// }
+
+
+const marks=1200
+
+if(marks<500){
+
+    console.log("marks is less than 500");
+    
+}
+
+
+else if(marks<750){
+
+    console.log("marks is less than 750");
+    
+}
+
+else if(marks<1000){
+
+    console.log("marks is less than 1000");
+    
+}
+
+
+// else{
+
+
+//     console.log("marks is greater than 1000");
+    
+// }
+
+const score=200
+// if(score<500){
+// const name="ashish"
+
+// console.log(`My name is ${name}`);
+
+
+// }
+// if(score<500) console.log("less than 500"),console.log("hii");
+
+// we can write statement with using scope but you have to write this in single line with ","
+
+const isUserLogged = true
+const debitcard=true
+
+if(isUserLogged && debitcard && 2>2){
+
+    console.log("You are allowed to buy courses");
+    
+}
+
+
+const isGoogleLogin= true
+const isEmailLogin=false
+
+if(isEmailLogin || isGoogleLogin){
+
+    console.log("Welcome to the homepage");
+
+}
+// && amd || -> these are logical operators   
+// && if anyone one expression is false then it will not check for others
+// for true it has to check for all the expression
+
+//  || if any expression is true it will not check for other 
+
+
+
+
+
+    
+
+
+// console.log(`My name is ${name}`);
+
