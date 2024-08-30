2cb15fe9bb79ca8c63fc460e017e6cbd769460b4
ashish <aashidare123@gmail.com>
2024-08-30 22:45:51 +0530

    control statement

---
+++ 04_Control_flow/switch.js
@@ -0,0 +1,61 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// const month = 3;
+// switch (month) {
+//   case 1:
+//     console.log("January");
+
+//     break;
+
+//   case 2:
+//     console.log("February");
+
+//     break;
+
+//   case 3:
+//     console.log("March");
+
+//     // break;
+
+//   case 4:
+//     console.log("April");
+
+//     // break;
+
+//   default:
+//     console.log("Invalid");
+// }
+
+// if we miss the break statement then case after that will be executed
+const month="jan"
+
+switch (month) {
+    case "jan":
+      console.log("January");
+  
+      break;
+  
+    case "feb":
+      console.log("February");
+  
+      break;
+  
+    case "mar":
+      console.log("March");
+  
+      break;
+  
+    case "apr":
+      console.log("April");
+  
+      break;
+  
+    default:
+      console.log("Invalid");
+  }
+  
---
+++ 04_Control_flow/truthy.js
@@ -0,0 +1,82 @@
+// const email= "ashish@gmail.com"
+// const email= " "
+// const email= []
+// const email= undefined
+//  const email= null
+//   const email= NaN
+
+// if(email){
+
+//     console.log("You got ur user");
+    
+// }
+
+// else{
+
+//     console.log(console.log("You dont have ur email"));
+
+    
+    
+    
+// }
+
+
+//falsy values
+
+// false, 0, -0,0n,"",undefined,null, NaN
+
+//truthy values
+
+// true,"false","0"," ",[],{},function(){}
+
+
+
+const email=[]
+
+// if(email.length==0){
+//     console.log("email is empty");
+    
+// }
+
+// else{
+//     console.log("email is not empty");
+    
+// }
+
+
+const obj={}
+
+// if(Object.keys(obj).length==0){
+//     console.log("obj is empty");
+    
+// }
+
+// else{
+
+//     console.log("obj is not empty");
+    
+// }
+
+
+// Nullish Coalescing Operator : (??) null undefined
+
+
+// const val = 5??10
+//  const val = null??5
+// const val = 5??undefined
+// const val = null??undefined??10
+//  const val = null??undefined
+
+// console.log(val);
+
+
+//Terniary Operator
+
+// condition? true:false;
+
+const iceTea= 800
+
+iceTea>1000?console.log("greater than 1000"):console.log("less than 1000");
+
+
+
