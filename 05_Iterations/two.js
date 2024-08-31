997239ebf282f3481410d9e585e80450c884a0be
ashish <aashidare123@gmail.com>
2024-08-31 23:33:58 +0530

    while and do while

---
+++ 05_Iterations/two.js
@@ -0,0 +1,53 @@
+let i=1
+
+
+// while (i<=10) {
+
+
+//     console.log(`Value is ${i}`);
+//     i++
+    
+    
+// }
+
+
+
+let marks=0
+
+// while (marks<=10) {
+
+
+//     console.log(`Marks : ${marks}`);
+
+
+//     marks=marks +2
+    
+    
+// }
+
+
+const myArray=["flash","superman","batman"]
+
+
+let arr=0;
+// while (arr<myArray.length) {
+
+//     console.log(`Value is ${myArray[arr]}`);
+
+//     arr++
+    
+
+
+    
+// }
+
+
+
+let j=11
+do{
+
+    console.log(`Value : ${j}`);
+    j++
+    
+
+}while(j<=10)
